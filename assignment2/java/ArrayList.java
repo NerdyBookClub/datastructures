4345d6a489f7277dc6bc7c33233a51baadea6519
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-14 21:34:52 -0400

    Added skeletons for Java Lists

---
+++ assignment2/java/ArrayList.java
@@ -0,0 +1,27 @@
+public class ArrayList<T> extends List<T> {
+
+    public void append(T value) {
+        return; // TODO: implement this
+    }
+
+    public void prepend(T value) {
+        return; // TODO: implement this
+    }
+
+    public void insert(T value, int index) {
+        return; // TODO: implement this
+    }
+
+    public boolean isEmpty() {
+        return true; // TODO: implement this
+    }
+
+    public int length() {
+        return 0; // TODO: implement this
+    }
+
+    public T get(int index) throws IndexOutOfBoundsException {
+        return null; // TODO: implement this
+    }
+
+}

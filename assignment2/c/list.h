d64af98db5975e267a6a98c0ff8aa5f36e6bb67e
Hawk Weisman <hawk@meteorcodelabs.com>
2015-06-14 21:28:17 -0400

    Added comments to the C header file for List

--- assignment2/c/list.h
+++ assignment2/c/list.h
@@ -1,3 +1,21 @@
 
-/* this is where you would change the list's element type */
-typedef int LIST_ELEMENT;
+/* this is where you can change the list's element type     */
+typedef int LIST_ELEM;
+
+struct list_struct;
+typedef *list_struct List;
+
+/* Returns true if the specified list is empty.             */
+int list_isEmpty(List l);
+
+/* Returns the length of the specified list.                */
+int list_length(List l);
+
+/* Adds the specified element to the beginning of the list. */
+void list_prepend(List l, LIST_ELEM);
+
+/* Adds the specified element to the end of the list.       */
+void list_append(List l, LIST_ELEM);
+
+/* Returns the element at the specified index into the list. */
+LIST_ELEM list_get(List l, int index);
